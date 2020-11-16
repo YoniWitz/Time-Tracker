6f73c5d9f0f2b7caa7f9c8ec1d0530f18bcccf7c
Yoni Witz <jonhirsh39@gmail.com>
2020-11-15 19:00:46 -0500

    added authors page and table data from database

---
+++ TimeTracker/Authors.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+
+
+namespace TimeTracker
+{
+    public partial class Authors : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
